d2d5e43d1a5dffd15abb3271ed3054cd181418be
Vadim Bezmelnitsyn <aero125@mail.ru>
2024-06-20 18:08:59 +0500

    Added reading variables from ENV and exact package versions

--- app/db.py
+++ app/db.py
@@ -1,15 +1,20 @@
+import os
+
 import psycopg2
 from psycopg2 import pool
 
+
 class DatabaseManager:
     def __init__(self):
-        DB_USER = "postgres"
-        DB_PASSWORD = "password"
+        DB_USER = os.getenv("POSTGRES_USER")
+        DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
         DB_HOST = "db"
-        DB_NAME = "restaurant_db"
+        DB_NAME = os.getenv("POSTGRES_DB")
 
         DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-        self.connection_pool = psycopg2.pool.SimpleConnectionPool(1, 20, dsn=DATABASE_URL)
+        self.connection_pool = psycopg2.pool.SimpleConnectionPool(
+            1, 20, dsn=DATABASE_URL
+        )
 
     def get_connection(self):
         return self.connection_pool.getconn()
@@ -17,7 +22,9 @@ class DatabaseManager:
     def release_connection(self, conn):
         self.connection_pool.putconn(conn)
 
-    def execute_query(self, sql_str: str, params: tuple | None = None, fetch: bool = False):
+    def execute_query(
+        self, sql_str: str, params: tuple | None = None, fetch: bool = False
+    ):
         conn = self.get_connection()
         try:
             with conn.cursor() as cursor:
