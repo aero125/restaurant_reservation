654cdf97642b823cf30d68e6381037011eb5f14a
Vadim Bezmelnitsyn <aero125@mail.ru>
2024-06-20 12:45:39 +0500

    Created database class

---
+++ app/db.py
@@ -0,0 +1,30 @@
+import psycopg2
+from psycopg2 import pool
+from typing import Tuple, Optional
+
+class DatabaseManager:
+    def __init__(self):
+        DB_USER = "postgres"
+        DB_PASSWORD = "password"
+        DB_HOST = "db"
+        DB_NAME = "restaurant_db"
+
+        DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+        self.connection_pool = psycopg2.pool.SimpleConnectionPool(1, 20, dsn=DATABASE_URL)
+
+    def get_connection(self):
+        return self.connection_pool.getconn()
+
+    def release_connection(self, conn):
+        self.connection_pool.putconn(conn)
+
+    def execute_query(self, sql_str: str, params: tuple | None = None, fetch: bool = False):
+        conn = self.get_connection()
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(sql_str, params or ())
+                conn.commit()
+                if fetch:
+                    return cursor.fetchall()
+        finally:
+            self.release_connection(conn)
